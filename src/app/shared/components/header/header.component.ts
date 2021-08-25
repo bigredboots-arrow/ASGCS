57cf3a5b8e28311db639bd758e18c7f0db1b96a2
bigredboots <bigredboots@gmail.com>
2021-08-25 09:16:04 -0600

    adding in mobile menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,6 +9,8 @@ import {
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { GlobalConstants } from '../../../common/global-constants';
+import { SidePanelService } from '../../../core/';
+import { SidePanelState } from '../../../core/';
 
 @Component({
   selector: 'app-header',
@@ -16,12 +18,17 @@ import { GlobalConstants } from '../../../common/global-constants';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  private _subscriptionsSubject$: Subject<void>;
+  public currentPanelState: SidePanelState;
+
+  constructor(private _sidePanelService: SidePanelService) {
+    this._subscriptionsSubject$ = new Subject<void>();
+  }
+
   @ViewChild('overview') overview: ElementRef;
   @ViewChild('filters') filters: ElementRef;
 
   breadCrumb = GlobalConstants.breadCrumb;
-
-  constructor() {}
 
   toggleSidebar() {
     this.overview.nativeElement.classList.toggle('sidebar-active');
@@ -33,5 +40,27 @@ export class HeaderComponent implements OnInit {
 
   myBreadCrumb = this.breadCrumb;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._sidePanelService.panelStateChanges
+      .pipe(takeUntil(this._subscriptionsSubject$))
+      .subscribe((state: SidePanelState) => (this.currentPanelState = state));
+  }
+
+  public handleSingleClicks(): void {
+    const width: number = window.innerWidth;
+    if (width < 768 && this.currentPanelState === SidePanelState.MOBILE) {
+      this._sidePanelService.changeState(SidePanelState.MOBILEOPEN);
+    } else if (this.currentPanelState === SidePanelState.MOBILEOPEN) {
+      this._sidePanelService.changeState(SidePanelState.MOBILE);
+    } else if (this.currentPanelState === SidePanelState.COLLAPSE) {
+      this._sidePanelService.changeState(SidePanelState.OPEN);
+    } else {
+      this._sidePanelService.changeState(SidePanelState.COLLAPSE);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptionsSubject$.next();
+    this._subscriptionsSubject$.complete();
+  }
 }

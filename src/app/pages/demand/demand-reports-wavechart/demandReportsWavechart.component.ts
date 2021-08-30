12f6190e72fdfe005fbef392aa3f8e8a85b678a3
bigredboots <bigredboots@gmail.com>
2021-08-30 11:23:40 -0600

    why broken?

--- src/app/pages/demand/demand-reports-wavechart/demandReportsWavechart.component.ts
+++ src/app/pages/demand/demand-reports-wavechart/demandReportsWavechart.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'demand-reports-wavechart',
- templateUrl: './demandReportsWavechart.component.html',
+  selector: 'demand-reports',
+  templateUrl: './demandReportsWavechart.component.html',
   styleUrls: ['./demandReportsWavechart.component.scss']
 })
 export class DemandReportsWavechartComponent {}

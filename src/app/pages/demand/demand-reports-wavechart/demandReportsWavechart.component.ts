364fc5acccb16b82dca6294c951890cead6b8e1f
bigredboots <bigredboots@gmail.com>
2021-08-30 09:58:50 -0600

    F this

--- src/app/pages/demand/demand-reports-wavechart/demandReportsWavechart.component.ts
+++ src/app/pages/demand/demand-reports-wavechart/demandReportsWavechart.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'demand-reports-wavechart',
-  templateUrl: './demandReportsWavechart.component.html',
+ templateUrl: './demandReportsWavechart.component.html',
   styleUrls: ['./demandReportsWavechart.component.scss']
 })
 export class DemandReportsWavechartComponent {}

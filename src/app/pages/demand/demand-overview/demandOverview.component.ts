ff7d4b003489247ccc753c1abe9a00bb3e3956cd
bigredboots <bigredboots@gmail.com>
2021-08-31 10:31:55 -0600

    more info

--- src/app/pages/demand/demand-overview/demandOverview.component.ts
+++ src/app/pages/demand/demand-overview/demandOverview.component.ts
@@ -1,8 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'demand-overview',
-   templateUrl: './demandOverview.component.html',
+  templateUrl: './demandOverview.component.html',
   styleUrls: ['./demandOverview.component.scss']
 })
-export class DemandOverviewComponent {}
+export class DemandOverviewComponent implements OnInit {
+  @ViewChild('overview') overview: ElementRef;
+
+  toggleMoreInfo() {
+    this.overview.nativeElement.classList.toggle('moreinfo-active');
+  }
+  ngOnInit(): void {}
+}

588e8d05d33120eef274d9f781f567c6512a2427
bigredboots <bigredboots@gmail.com>
2021-09-07 13:43:25 -0600

    Hubs near complete

--- src/app/pages/hubs/hubs/hubs.component.ts
+++ src/app/pages/hubs/hubs/hubs.component.ts
@@ -6,5 +6,10 @@ import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./hubs.component.scss']
 })
 export class HubsComponent {
+  @ViewChild('flocationsoverview') flocationsoverview: ElementRef;
+
+  FLocationsMoreInfo() {
+    this.flocationsoverview.nativeElement.classList.toggle('moreinfo-active');
+  }
   ngOnInit(): void {}
 }

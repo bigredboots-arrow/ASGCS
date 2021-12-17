03da5789c107db1c232cbc18867efcbcb44579ec
bigredboots <bigredboots@gmail.com>
2021-12-17 12:18:56 -0700

    Its working! kinda

--- src/app/shared/components/charts/donut-chart/donut-chart.component.ts
+++ src/app/shared/components/charts/donut-chart/donut-chart.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-donut-chart',
@@ -68,75 +74,88 @@ export class DonutComponent implements OnInit {
   ngOnInit(): void {}
 
   onClickMe() {
-    alert ('bork');
-    series: [
-      {
-        holeSize: 65,
+    this.chartConfig = {
+      chartArea: {
+        background: 'transparent',
+        height: 280,
+        top: 0,
+        padding: 0,
         margin: 0,
-        name: 'parts',
-        type: 'donut',
-        border: {
-          width: 2,
-          color: 'black',
-          dashType: 'solid',
-        },
-        explodeField: 'explode',
-
-        data: [
-          {
-            category: 'Data Point 1',
-            value: 35,
-            color: 'rgba(6, 90, 163, 1)',
-            explode: false,
-          },
-          {
-            category: 'Data Point 2',
-            value: 25,
-            color: 'rgba(6, 90, 163, .8)',
-          },
-          {
-            category: 'Data Point 3',
-            value: 20,
-            color: 'rgba(6, 90, 163, .6)',
-          },
-          {
-            category: 'Data Point 4',
-            value: 10,
-            color: 'rgba(6, 90, 163, .4)',
-          },
-          {
-            category: 'Other',
-            value: 5,
-            color: 'rgba(6, 90, 163, .2)',
-          },
-          {
-            category: 'Data Point 1',
-            value: 35,
-            color: 'rgba(6, 90, 163, 1)',
-            explode: false,
-          },
-          {
-            category: 'Data Point 2',
-            value: 25,
-            color: 'rgba(6, 90, 163, .8)',
-          },
-          {
-            category: 'Data Point 3',
-            value: 20,
-            color: 'rgba(6, 90, 163, .6)',
-          },
-          {
-            category: 'Data Point 4',
-            value: 10,
-            color: 'rgba(6, 90, 163, .4)',
-          },
-          {
-            category: 'Other',
-            value: 5,
-            color: 'rgba(6, 90, 163, .2)',
-          },
-        ],
+        width: 280,
+        autoFit: false,
       },
-    ];
+      legend: {
+        visible: false,
+      },
+      series: [
+        {
+          holeSize: 65,
+          margin: 0,
+          name: 'parts',
+          type: 'donut',
+          border: {
+            width: 2,
+            color: 'black',
+            dashType: 'solid',
+          },
+          explodeField: 'explode',
+
+          data: [
+            {
+              category: 'Data Point 1',
+              value: 35,
+              color: 'rgba(6, 90, 163, 1)',
+              explode: false,
+            },
+            {
+              category: 'Data Point 2',
+              value: 25,
+              color: 'rgba(6, 90, 163, .9)',
+            },
+            {
+              category: 'Data Point 3',
+              value: 20,
+              color: 'rgba(6, 90, 163, .8)',
+            },
+            {
+              category: 'Data Point 4',
+              value: 10,
+              color: 'rgba(6, 90, 163, .7)',
+            },
+            {
+              category: 'Data Point 5',
+              value: 5,
+              color: 'rgba(6, 90, 163, .6)',
+            },
+            {
+              category: 'Data Point 6',
+              value: 35,
+              color: 'rgba(6, 90, 163, 5)',
+              explode: false,
+            },
+            {
+              category: 'Data Point 7',
+              value: 25,
+              color: 'rgba(6, 90, 163, .4)',
+            },
+            {
+              category: 'Data Point 8',
+              value: 20,
+              color: 'rgba(6, 90, 163, .3)',
+            },
+            {
+              category: 'Data Point 9',
+              value: 10,
+              color: 'rgba(6, 90, 163, .2)',
+            },
+            {
+              category: 'Other',
+              value: 5,
+              color: 'rgba(6, 90, 163, .1)',
+            },
+          ],
+        },
+      ],
+    };
   }
 }

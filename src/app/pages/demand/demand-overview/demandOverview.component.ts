1c7d65cb9b219c6cb9a29a10195d4782ffdc7dd2
bigredboots <bigredboots@gmail.com>
2021-12-17 11:35:37 -0700

    little tweaks

--- src/app/pages/demand/demand-overview/demandOverview.component.ts
+++ src/app/pages/demand/demand-overview/demandOverview.component.ts
@@ -15,29 +15,31 @@ export class DemandOverviewComponent implements OnInit {
   public borderOptions = {
     color: '#000000',
     dashType: 'solid',
-    width: 2,
-    radius: 10,
+    width: 1,
+    radius: 5,
   };
   public seriesDefaults = {
-    overlay: { gradient: 'sharpGlass'}
+    overlay: { gradient: 'sharpGlass' },
   };
 
   @ViewChild('partoverview') partoverview: ElementRef;
   @ViewChild('fsiteoverview') fsiteoverview: ElementRef;
-  @ViewChild('totaldemandoverview') totaldemandoverview: ElementRef;
   @ViewChild('flocationsoverview') flocationsoverview: ElementRef;
+  @ViewChild('ipartoverview') ipartoverview: ElementRef;
+  @ViewChild('ifsiteoverview') ifsiteoverview: ElementRef;
+  @ViewChild('iflocationsoverview') iflocationsoverview: ElementRef;
 
   PartOverviewtoggleMoreInfo() {
     this.partoverview.nativeElement.classList.toggle('moreinfo-active');
+    this.ipartoverview.nativeElement.classList.toggle('moreinfo-active');
   }
   FsitetoggleMoreInfo() {
     this.fsiteoverview.nativeElement.classList.toggle('moreinfo-active');
-  }
-  TotalDemandMoreInfo() {
-    this.totaldemandoverview.nativeElement.classList.toggle('moreinfo-active');
+    this.ifsiteoverview.nativeElement.classList.toggle('moreinfo-active');
   }
   FLocationsMoreInfo() {
     this.flocationsoverview.nativeElement.classList.toggle('moreinfo-active');
+    this.iflocationsoverview.nativeElement.classList.toggle('moreinfo-active');
   }
   ngOnInit(): void {}
 }

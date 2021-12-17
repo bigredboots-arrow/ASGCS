d565ddca5f1e3e2408bac6274ea6366a19c5ad3c
bigredboots <bigredboots@gmail.com>
2021-12-17 12:40:27 -0700

    WOW.  Did it?  Hope it saves

--- src/app/shared/components/charts/donut-chart/donut-chart.component.ts
+++ src/app/shared/components/charts/donut-chart/donut-chart.component.ts
@@ -72,8 +72,10 @@ export class DonutComponent implements OnInit {
     ],
   };
   ngOnInit(): void {}
+  @ViewChild('donutoverview') donutoverview: ElementRef;
 
   onClickMe() {
+    this.donutoverview.nativeElement.classList.toggle('active');
     this.chartConfig = {
       chartArea: {
         background: 'transparent',
@@ -103,55 +105,115 @@ export class DonutComponent implements OnInit {
           data: [
             {
               category: 'Data Point 1',
-              value: 35,
+              value: 30,
               color: 'rgba(6, 90, 163, 1)',
               explode: false,
             },
             {
               category: 'Data Point 2',
-              value: 25,
+              value: 27,
               color: 'rgba(6, 90, 163, .9)',
             },
             {
               category: 'Data Point 3',
-              value: 20,
+              value: 22,
               color: 'rgba(6, 90, 163, .8)',
             },
             {
               category: 'Data Point 4',
-              value: 10,
+              value: 15,
               color: 'rgba(6, 90, 163, .7)',
             },
             {
               category: 'Data Point 5',
-              value: 5,
+              value: 11,
               color: 'rgba(6, 90, 163, .6)',
             },
             {
               category: 'Data Point 6',
-              value: 35,
-              color: 'rgba(6, 90, 163, 5)',
+              value: 8,
+              color: 'rgba(6, 90, 163, .5)',
               explode: false,
             },
             {
               category: 'Data Point 7',
-              value: 25,
+              value: 6,
               color: 'rgba(6, 90, 163, .4)',
             },
             {
               category: 'Data Point 8',
-              value: 20,
+              value: 5,
               color: 'rgba(6, 90, 163, .3)',
             },
             {
               category: 'Data Point 9',
-              value: 10,
+              value: 4,
               color: 'rgba(6, 90, 163, .2)',
             },
             {
               category: 'Other',
-              value: 5,
+              value: 2,
               color: 'rgba(6, 90, 163, .1)',
+            },
+          ],
+        },
+      ],
+    };
+  }
+  onClickMeClicked() {
+    this.donutoverview.nativeElement.classList.toggle('active');
+    this.chartConfig = {
+      chartArea: {
+        background: 'transparent',
+        height: 280,
+        top: 0,
+        padding: 0,
+        margin: 0,
+        width: 280,
+        autoFit: false,
+      },
+      legend: {
+        visible: false,
+      },
+      series: [
+        {
+          holeSize: 65,
+          margin: 0,
+          name: 'parts',
+          type: 'donut',
+          border: {
+            width: 2,
+            color: 'black',
+            dashType: 'solid',
+          },
+          explodeField: 'explode',
+
+          data: [
+            {
+              category: 'Data Point 1',
+              value: 36,
+              color: 'rgba(6, 90, 163, 1)',
+              explode: false,
+            },
+            {
+              category: 'Data Point 2',
+              value: 26,
+              color: 'rgba(6, 90, 163, .8)',
+            },
+            {
+              category: 'Data Point 3',
+              value: 21,
+              color: 'rgba(6, 90, 163, .6)',
+            },
+            {
+              category: 'Data Point 4',
+              value: 11,
+              color: 'rgba(6, 90, 163, .4)',
+            },
+            {
+              category: 'Other',
+              value: 6,
+              color: 'rgba(6, 90, 163, .2)',
             },
           ],
         },
